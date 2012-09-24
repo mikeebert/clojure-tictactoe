8a74a2e0f4e194ca74aec928d06d4a248ba1b88b
mikeebert <mike@mikeebert.net>
2012-09-24 14:40:25 -0500

    Adds try-catch for invalid move input. Adds with-in-str mocking for input tests.

--- spec/tictactoe/consoleui_spec.clj
+++ spec/tictactoe/consoleui_spec.clj
@@ -6,21 +6,39 @@
             [4 5 6]
             [7 8 9]])
 
+(def mock-player "x")
+
+(defn make-input [coll]
+  (apply str (interleave coll (repeat "\n"))))
+
 (describe "Console"
+
+  (it "should prompt a player for a move"
+    (with-redefs [prompt-for-move (fn [x] x)]
+      (should= "x" (prompt-for-move "x"))))
 
   (it "should convert a string entry into an integer"
     (should= (parse-int "5") 5))
 
-  (it "shoud reject a non integer")
+  (it "shoud reject a non integer and ask for another move"
+    (with-redefs [println (constantly nil)]
+      (with-in-str (make-input '("nine" 3))
+        (should= 3 (get-move mock-player [1 2 3])))))
 
-  (it "should reject an ineligible move")
-    ;(with-redefs [read-line (1 "1")
-                  ;read-line (1 "9")]
-      ;(should= (get-move [1 9]) 9)))
+  (it "should accept a valid move"
+    (with-redefs [println (constantly nil)
+                  read-line (constantly "9")]
+      (should= 9 (get-move "x" [1 9]))))
+
+  (it "should reject an invalid move"
+    (with-redefs [println (constantly nil)]
+      (with-in-str (make-input '(2 9))
+        (should= 9 (get-move "x" [1 9])))))
 
   (it "should get a move"
-    (with-redefs [read-line (constantly "1")]
-      (should= 1 (get-move [1 2 3]))))
+    (with-redefs [println (constantly nil)
+                  read-line (constantly "1")]
+      (should= 1 (get-move "x" [1 2 3]))))
   
   (it "should display the board"
     (with-redefs [println (constantly true)]
--- src/tictactoe/consoleui.clj
+++ src/tictactoe/consoleui.clj
@@ -7,16 +7,23 @@
 (defn begin []
   (println "Hello and welcome to tic-tac-toe."))
 
-(defn prompt-for-move []
-  (println "Please make your next move:"))
+(defn prompt-for-move [player]
+  (println (str player " player, please make your next move:")))
 
 (defn parse-int [string]
   (Integer. (re-find #"[0-9]" string)))
 
-(defn get-move [spaces]
-  (prompt-for-move)
-  (let [input (parse-int (read-line))]
-    (if (some #(= input %) spaces)
-      input
-      (get-move spaces))))
+(defn get-move [player spaces]
+  (prompt-for-move player)
+  (try 
+    (let [input (parse-int (read-line))]
+      (if (some #(= input %) spaces)
+        input
+        (do (println "Not a valid move.") (get-move player spaces))))
+    (catch Exception e (get-move player spaces))))
 
+(defn winning-message [player]
+  (println (str player " wins!")))
+
+(defn tie-message []
+  (println "Cat's game."))
