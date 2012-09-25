02524e5288e76cb82ce6a4b51416200436ec27e6
mikeebert <mike@mikeebert.net>
2012-09-25 16:53:26 -0500

    Start of minimax score

--- spec/tictactoe/ai_spec.clj
+++ spec/tictactoe/ai_spec.clj
@@ -2,6 +2,24 @@
   (:require [speclj.core :refer :all]
             [tictactoe.ai :refer :all]))
 
+(def one-move-x-win [["x" "x" 3]
+                   ["x" "o" "o"]
+                   ["o" "x" "o"]])
+
+(def won-board [["x" "x" "x"]
+                ["x" "o" "o"]
+                ["o" "x" "o"]])
+
+(def draw-board [["x" "o" "x"]
+                ["o" "x" "x"]
+                ["o" "x" "o"]])
+
+(def two-move-o-win [["x" "o" 3]
+                     ["o" 5 "o"]
+                     ["o" "x" "o"]] )
+
+;(def max-player {:symbol "x" :starting-score -5})
+;(def min-player {:symbol "o" :starting-score 5})
 
 (describe "AI"
   
@@ -15,4 +33,28 @@
     (let [move (random-ai-move [[1 "x" 3]
                                 [4 "o" 6]
                                 ["x" 8 9]])]
-    (should (some #(= move %) [1 3 4 6 8 9])))))
+    (should (some #(= move %) [1 3 4 6 8 9]))))
+  
+  (it "should score a won board for a player"
+    (should= 99 (value won-board "x" "o" 1)))
+
+  (it "should score a won board for an opponent"
+    (should= -99 (value won-board "o" "x" 1)))
+
+  (it "should score a draw board"
+    (should= 0 (value draw-board "o" "x" 1)))
+
+  (it "should not score a game in progress"
+    (should= nil (value one-move-x-win "o" "x" 6)))
+
+  (it "should return a won board with a score"
+    (should= 93 (minimax-score "x" "o" won-board 7)))
+
+  (it "should return a cat's game board with a score"
+    (should= 0 (minimax-score "x" "o" draw-board 7)))
+
+  (it "should score for a win in one move"
+    (should= 99 (minimax-score "x" "o" one-move-x-win 0)))
+  
+  (it "should score for a loss in two moves"
+    (should= -98 (minimax-score "x" "o" two-move-o-win 0))))
--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -1,5 +1,5 @@
 (ns tictactoe.ai
-  (:require [tictactoe.board :refer [available-spaces]]))
+  (:require [tictactoe.board :refer [winner full? available-spaces place-move]]))
 
 (defn random-ai-move [board] 
   (rand-nth (available-spaces board)))
@@ -7,3 +7,43 @@
 (defn ai-move [p1-symbol p2-symbol board]
   (random-ai-move board))
 
+;(defn minimax_move [p1_symbol p2_symbol board]
+  ;(let [spaces-index 0
+        ;alpha -100
+        ;beta 100
+        ;depth 0
+        ;best-score-for-max -5
+        ;best-score-for-min 5
+        ;possible-moves []
+        ;new-board (place-move (nth (available-spaces board) spaces-index) p1_symbol board)
+        ;score (minimax_score new-board p1_symbol alpha beta (inc depth))]
+    ;(if (= score best-score-for-max)
+      ;(do (conj possible-moves (nth (available-spaces board) spaces-index)) )
+      ;)))
+;
+(defn win [player board depth]
+  (if (= player (winner board))
+    (- 100 depth)))
+
+(defn loss [opponent board depth]
+  (if (= opponent (winner board))
+    (do (println "it got here") (+ -100 depth))))
+
+(defn draw [board]
+  (if (full? board) 0))
+
+(defn value [board player opponent depth]
+  (or (win player board depth)
+      (loss opponent board depth)
+      (draw board)))
+ 
+(defn minimax-score 
+  ([max-player min-player board depth] (minimax-score {:piece max-player :starting-score -5} 
+                                                      {:piece min-player :starting-score 5} 
+                                                      board (available-spaces board)
+                                                      depth -100 100))
+  ([player opponent board spaces depth alpha beta]
+    (or (value board (:piece player) (:piece opponent) depth)
+        (let [new-board (place-move (first spaces) (:piece player) board)]
+          (or (value new-board (:piece player) (:piece opponent) (inc depth))
+              (minimax-score opponent player new-board (rest spaces) (inc depth) alpha beta))))))
