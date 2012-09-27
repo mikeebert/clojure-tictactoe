10a7a42ee3ffc9a61aea1ea4e0af924ff208f6bc
mikeebert <mike@mikeebert.net>
2012-09-27 17:58:38 -0500

    Comments out game class in preparation for deletion

--- spec/tictactoe/game_spec.clj
+++ spec/tictactoe/game_spec.clj
@@ -1,29 +1,29 @@
-(ns tictactoe.game-spec
-  (:require [speclj.core :refer :all]
-            [tictactoe.game :refer :all]
-            [tictactoe.board :refer [winner place-move]]
-            [tictactoe.consoleui :as console]
-            [tictactoe.consoleui :refer [display-board get-human-move]]))
+;(ns tictactoe.game-spec
+  ;(:require [speclj.core :refer :all]
+            ;[tictactoe.game :refer :all]
+            ;[tictactoe.board :refer [winner place-move]]
+            ;[tictactoe.consoleui :as console]
+            ;[tictactoe.consoleui :refer [display-board get-human-move]]))
 
-(def mock-board [[][][]])
+;(def mock-board [[][][]])
 
-(describe "Game"
+;(describe "Game"
   
-  ;(it "should tell the UI to begin"
-    ;(with-redefs [console/begin (constantly true)]
-    ;(should= true (start))))
+  ;;(it "should tell the UI to begin"
+    ;;(with-redefs [console/begin (constantly true)]
+    ;;(should= true (start))))
   
-  (it "should send the board to the console"
-    (with-redefs [console/display-board (fn [x] x)]
-      (should= [1 2 3] (display-board [1 2 3]))))
+  ;(it "should send the board to the console"
+    ;(with-redefs [console/display-board (fn [x] x)]
+      ;(should= [1 2 3] (display-board [1 2 3]))))
   
-  (it "should start a game loop that gets input and checks for a winner"
-    (with-redefs [get-human-move (fn [player spaces] nil)
-                  println (constantly true)]
-      (should= true (game-loop mock-board "x"))))
+  ;(it "should start a game loop that gets input and checks for a winner"
+    ;(with-redefs [get-human-move (fn [player spaces] nil)
+                  ;println (constantly true)]
+      ;(should= true (game-loop mock-board "x"))))
   
-  (it "can return the next player"
-    (should= "X" (next-up "O")))
+  ;(it "can return the next player"
+    ;(should= "X" (next-up "O")))
   
-  (it "can return the next o player"
-    (should= "O" (next-up "X"))))
+  ;(it "can return the next o player"
+    ;(should= "O" (next-up "X"))))
--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,25 +1,25 @@
-(ns tictactoe.game
-  (:require [tictactoe.consoleui :refer [greeting display-board get-human-move 
-                                         winning-message tie-message]]
-            [tictactoe.board :refer [available-spaces new-board place-move winner]]))
+;(ns tictactoe.game
+  ;(:require [tictactoe.consoleui :refer [greeting display-board get-human-move 
+                                         ;winning-message tie-message]]
+            ;[tictactoe.board :refer [available-spaces new-board place-move winner]]))
 
 
-(defn next-up [player]
-  (if (= "X" player)
-    "O"
-    "X"))
+;(defn next-up [player]
+  ;(if (= "X" player)
+    ;"O"
+    ;"X"))
 
-(defn game-loop [board player]
-  (let [move (get-human-move player (available-spaces board))
-        game-board (place-move move player board)]
-    (display-board game-board)
-    (if-let [winning-player (winner game-board)]
-      (winning-message winning-player)
-      (if (empty? (available-spaces game-board))
-        (tie-message)
-        (game-loop game-board (next-up player))))))
+;(defn game-loop [board player]
+  ;(let [move (get-human-move player (available-spaces board))
+        ;game-board (place-move move player board)]
+    ;(display-board game-board)
+    ;(if-let [winning-player (winner game-board)]
+      ;(winning-message winning-player)
+      ;(if (empty? (available-spaces game-board))
+        ;(tie-message)
+        ;(game-loop game-board (next-up player))))))
 
-(defn start []
-  (greeting)
-  (display-board new-board)
-  (game-loop new-board "X"))
+;(defn start []
+  ;(greeting)
+  ;(display-board new-board)
+  ;(game-loop new-board "X"))
